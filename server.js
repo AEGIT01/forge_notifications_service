f8aa220ac5acad0200664402cb8003f3704fc2ab
Ahmed3mad <3umduck@gmail.com>
2023-01-16 12:50:41 +0200

    firebase database

--- server.js
+++ server.js
@@ -4,56 +4,83 @@ import cors from 'cors';
 import helmet from 'helmet';
 import { initializeApp, applicationDefault } from 'firebase-admin/app';
 import { getMessaging } from 'firebase-admin/messaging';
+import { getDatabase } from 'firebase-admin/database';
 
-initializeApp({
+const fb = initializeApp({
     credential: applicationDefault(),
+    databaseURL: 'https://laravel-forge-ac6ae-default-rtdb.firebaseio.com/'
 });
+
+const db = getDatabase(fb);
 
 const app = express();
 
 app.use(helmet());
 
 app.use(bodyParser.urlencoded({
-    extended:true,
+    extended: true,
 }));
 
 app.use(bodyParser.json());
 
 app.use(cors());
 
-var fcmTokens = [];
+async function getFcmTokens() {
+    return await db.ref('tokens').once('value').then((snapshot) => snapshot.val() ?? []);
+};
+// function setTokens(tokens) {
+//     db.ref("tokens").set(tokens, function (error) {
+//         if (error) {
+//             console.log("Failed with error: " + error)
+//         } else {
+//             console.log("success")
+//         }
+//     });
+// }
 
-app.get('/', (req, res) => {
-    res.send('Hello');
-});
+// db.ref('tokens').on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//     if (val == null) {
+//         fcmTokens = [];
+//     } else {
+//         fcmTokens = val;
+//     }
+// });
 
-app.post('/fcm_token', (req, res) => {
-    const token = req.body.token;
-    if(!fcmTokens.includes(token)){
-        fcmTokens = [...fcmTokens, token];
-    }
-    res.send({
-        "status": "success",
-    });
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello');
+// });
 
-app.delete('/fcm_token', (req, res) => {
-    const token = req.body.token;
-    if(fcmTokens.includes(token)){
-        fcmTokens = [...fcmTokens.filter((e) => e != token)];
-    }
-    res.send({
-        "status": "success",
-    });
-});
+// app.post('/fcm_token', (req, res) => {
+//     const token = req.body.token;
+//     if (!fcmTokens.includes(token)) {
+//         setTokens([...fcmTokens, token]);
+//     }
+//     res.send({
+//         "status": "success",
+//     });
+// });
 
-app.get('/fcm_tokens', (req, res) => {
-    res.send({
-        "tokens": fcmTokens,
-    });
-});
+// app.delete('/fcm_token', (req, res) => {
+//     const token = req.body.token;
+//     if (fcmTokens.includes(token)) {
+//         setTokens([...fcmTokens.filter((e) => e != token)]);
+//     }
+//     res.send({
+//         "status": "success",
+//     });
+// });
 
-app.post('/new_deployment', (req, res) => {
+// app.get('/fcm_tokens', async (req, res) => {
+//     const fcmTokens = await getFcmTokens();
+//     res.send({
+//         "tokens": fcmTokens,
+//     });
+// });
+
+app.post('/new_deployment', async (req, res) => {
+    const fcmTokens = await getFcmTokens();
     if (fcmTokens.length > 0) {
         const body = req.body;
         const data = {
